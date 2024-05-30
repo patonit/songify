b5b2d30cf59b4a240868ca4768758ec445003669
patonit <rafal.paton@gmail.com>
2024-05-30 13:44:57 +0200

    first commit

---
+++ src/main/java/com/songify/SongifyApplication.java
@@ -0,0 +1,15 @@
+package com.songify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SongifyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SongifyApplication.class, args);
+        System.out.println("Hello World!");
+    }
+
+}
+
